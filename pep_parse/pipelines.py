c993a7f32ae123c61d6a0088ebbeb6f2b2eb60cd
Андрей Филиппович <andrejfilippovic8@gmail.com>
2023-04-11 15:26:06 +0300

    trying to fix

---
+++ pep_parse/pipelines.py
@@ -0,0 +1,30 @@
+import datetime as dt
+
+from .settings import DATETIME_FORMAT, BASE_DIR, RESULT_DIR
+
+
+class PepParsePipeline:
+
+    def open_spider(self, spider):
+        self.results = {}
+
+    def process_item(self, item, spider):
+        if item['status'] not in self.results:
+            self.results[item['status']] = 1
+        else:
+            self.results[item['status']] += 1
+        return item
+
+    def close_spider(self, spider):
+        results_dir = BASE_DIR / RESULT_DIR
+        now = dt.datetime.now()
+        now_format = now.strftime(DATETIME_FORMAT)
+        filename = f'status_summary_{now_format}.csv'
+        file_path = results_dir / filename
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write('Статус,Количество\n')
+            total = 0
+            for key, value in self.results.items():
+                total += value
+                f.write(f'{key},{value}\n')
+            f.write(f'Total,{total}\n')
